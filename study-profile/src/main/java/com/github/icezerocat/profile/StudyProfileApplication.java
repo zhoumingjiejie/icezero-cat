c55e128303fb17b5a0cf73d61dd246dd78822a66
zhoumingjie <996586523@qq.com>
2020-12-03 15:16:21 +0800

    【添加】1、多环境版本控制

---
+++ study-profile/src/main/java/com/github/icezerocat/profile/StudyProfileApplication.java
@@ -0,0 +1,13 @@
+package com.github.icezerocat.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyProfileApplication.class, args);
+    }
+
+}
